a8c851548c1f39c87c8532c30267add4b9bbaa71
Tianrui Guan <502112826@qq.com>
2024-11-07 13:18:02 -0500

    fix bugs

--- maploc/data/__init__.py
+++ maploc/data/__init__.py
@@ -1,8 +1,6 @@
 from .kitti.dataset import KittiDataModule, KittiLidarModule
-from .mapillary.dataset import MapillaryDataModule
 from .carla.dataset import CarlaDataModule
 
-modules = {"mapillary": MapillaryDataModule, 
-           "kitti": KittiDataModule, 
+modules = {"kitti": KittiDataModule, 
            "kitti_lidar": KittiLidarModule, 
            "carla": CarlaDataModule}
